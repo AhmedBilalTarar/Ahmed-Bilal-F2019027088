d6cfe30cbd5ff66e9ebc15470b500a6d5aa44e1b
Ahmed Bilal <ahmedbilaltarar@gmail.com>
2021-04-03 18:15:38 +0500

    My First Commit

---
+++ src/assignment_f2019027088/Assignment_F2019027088.java
@@ -0,0 +1,17 @@
+
+package assignment_f2019027088;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Assignment_F2019027088 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
